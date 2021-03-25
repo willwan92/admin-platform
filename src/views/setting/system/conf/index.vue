a83b7022b63894f46b505fc6b497683103d41051
willwan92 <wilson3cy@163.com>
2021-03-25 14:17:41 +0800

    配置管理页面

--- src/views/setting/system/conf/index.vue
+++ src/views/setting/system/conf/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div style="padding:30px;">
-    配置管理
-  </div>
+  <el-card class="card" header="配置管理">
+      <el-form v-model="timeForm" label-width="100px">
+        <el-form-item label="导出加密">
+          <el-radio-group v-model="encrypted">
+            <el-radio :label="1">是</el-radio>
+            <el-radio :label="0">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">导出配置</el-button>
+          <el-upload
+            class="upload"
+            action=""
+            :auto-upload="false">
+            <el-button>导入配置</el-button>
+          </el-upload>
+          <el-button>恢复默认配置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      encrypted: 1
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.upload {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
